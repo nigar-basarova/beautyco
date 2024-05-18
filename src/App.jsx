cee8396a2922659d6376f32873963fa493a62a09
nigar-basarova <nigarbasarova@gmail.com>
2024-05-18 05:20:36 +0400

    beautyco

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import './App.css'
+import '../public/assets/css/Navbar.css'
+// komponentler gelecek
+import NavbarKomponenti from './components/Navbar'
+function App() {
+ 
+
+  return (
+ < NavbarKomponenti/>
+  )
+}
+
+export default App
