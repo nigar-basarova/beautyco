eca12829d830a35139b475bc1186bd8c1328843d
nigar-basarova <nigarbasarova@gmail.com>
2024-05-18 15:47:16 +0400

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
 import './App.css'
 import '../public/assets/css/Navbar.css'
+import '../public/assets/css/Introduction.css'
 // komponentler gelecek
 import NavbarKomponenti from './components/Navbar'
+import Introduction from './components/Introduction'
 function App() {
  
 
   return (
+    <>
  < NavbarKomponenti/>
+<Introduction/>
+ </>
   )
 }
 
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <div className='intro'>
+        <div className="container d-flex">
+
+        <div className="intro-left-side">
+
+<h2 className=''> Hydrated skin in 3 easy steps</h2>
+<p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique duis Cursus mi quis viverra ornare.
+
+</p>
+<form className="d-flex gap-2" role="search">
+<button className="btn-second btn-outline text-white " type="submit"> Shop now</button>
+ <button className="btn-first btn-outline " type="submit">    Our story
+            </button>
+          </form>
+          <p>Trusted by the world's best companies</p>
+          
+
+        
+        <img className='p-2' src="assets/images/logos/logo1.svg" alt="" />
+          <img className='p-2' src="assets/images/logos/logo2.svg" alt="" />
+          <img className='p-2'  src="assets/images/logos/logo3.svg" alt="" />
+        
+          
+            </div>
+
+            <div className="intro-right-side">
+<img src="assets/images/man.svg" alt="" />
+
+            </div>
+
+    
+        </div>
+    </div>
+  )
+}
+
+export default Introduction
