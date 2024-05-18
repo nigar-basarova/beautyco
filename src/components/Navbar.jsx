ca302e98128763c273a65f5c27ce0695fe83248c
nigar-basarova <nigarbasarova@gmail.com>
2024-05-18 15:46:57 +0400

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg  mt-2">
-      <div className="container">
+    <nav className="navbar navbar-expand-lg  mt-2 ">
+      <div className="container ">
         {/* <a className="navbar-brand" href="#">Navbar</a> */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
